1cdb547dfc4226f24cdb5c66311203ca26330cbf
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-16 16:24:54 -0600

    validate inputs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+var add = document.getElementsByClassName('add')
+
+add[0].addEventListener('click', validateAge);
+
+function validateAge() {
+  // var input = document.input['age'].value;
+  debugger
+  // console.log(input);
+}
