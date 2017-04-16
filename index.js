ad7c1381388c034e743bce5b9ce36f1dfcb2f46f
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-16 17:44:44 -0600

    relationship validation

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ add[0].addEventListener('click', validateAge);
 
 function validateAge() {
   var input = document.getElementsByName('age')[0].value;
-  console.log(input);
+  var rel = document.getElementsByName('rel')[0].value;
+  var inputInt = parseInt(input)
   if (input === '') {
     alert("Age must be filled out");
+    return false
+  }
+  if (inputInt < 1) {
+    alert("Age must be greater than zero");
+    return false
+  }
+  if (rel === '') {
+    alert("You must select this person's relationship");
     return false
   }
 }
