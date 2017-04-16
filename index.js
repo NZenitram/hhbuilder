b75c944ae13b4666acd44ba731c668a3a420a830
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-16 16:33:32 -0600

    age validation alerts

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var add = document.getElementsByClassName('add')
 add[0].addEventListener('click', validateAge);
 
 function validateAge() {
-  // var input = document.input['age'].value;
-  debugger
-  // console.log(input);
+  var input = document.getElementsByName('age')[0].value;
+  console.log(input);
+  if (input === '') {
+    alert("Age must be filled out");
+    return false
+  }
 }
