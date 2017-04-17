fea932fb3eace347cf1f206718f2a5407d22d88e
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-04-16 19:41:28 -0600

    all form inputs return values

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-var add = document.getElementsByClassName('add')
+// objects
+var Person = function(age, relationship, smoker) {
+  this.age = age;
+  this.relationship = relationship;
+  this.smoker = smoker;
+}
 
+// event listeners
+var add = document.getElementsByClassName('add')
 add[0].addEventListener('click', validateAge);
 
+// form validation script
 function validateAge() {
   var input = document.getElementsByName('age')[0].value;
   var rel = document.getElementsByName('rel')[0].value;
+  var smoker = document.getElementsByName('smoker')[0].checked;
+
   var inputInt = parseInt(input)
   if (input === '') {
     alert("Age must be filled out");
@@ -18,4 +28,9 @@ function validateAge() {
     alert("You must select this person's relationship");
     return false
   }
+  saveToLocalStorage(input, rel, smoker)
+}
+
+function saveToLocalStorage(input, rel, smoker) {
+  debugger
 }
---
+++ person.js
@@ -0,0 +1,5 @@
+var Person = function(age, relationship, smoker) {
+  this.age = age;
+  this.relationship = relationship;
+  this.smoker = smoker;
+}
